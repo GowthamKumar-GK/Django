4895c2852506bbaef4bdaf980bfde869c81de17f
GowthamKumar-GK <gkgowtham005@gmail.com>
2025-06-19 12:07:27 +0530

    commit

--- Instagram Clone/social_book/settings.py
+++ Instagram Clone/social_book/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-y6on4&^y1+^c$5_h4eh-i2fz-c6jmagos9$5b!o!%8=08s#!tm
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
@@ -130,3 +130,7 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
+import os
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
